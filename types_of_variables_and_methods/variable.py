88a879e37d71513f7ee459fc64f66bdc54a29633
ppopow <pawel.popow@gmail.com>
2022-08-17 14:05:36 +0100

    new five commit

---
+++ types_of_variables_and_methods/variable.py
@@ -0,0 +1,21 @@
+class Variable:
+    def __init__(self):
+        self.public = 10
+        self._protected = 20
+        self.__private1 = 30
+        self.__privacy_double = self.__private1 * 2  # jest zależność między zmiennymi
+
+    def privat_fetch2(self):
+        return self.__privacy_double
+
+    def privat_fetch1(self):
+        return self.__private1
+
+    def private_var1(self, new_value):  # zmieniamy dwie waratości
+        self.__private1 = new_value
+        self.__privacy_double = self.__private1 * 2
+
+
+if __name__ == '__main__':
+    zm = Variable()
+    print(zm)
