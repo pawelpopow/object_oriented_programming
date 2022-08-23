ae92dac76c1ddb33739a40cc4417ad0f49ca46cc
ppopow <pawel.popow@gmail.com>
2022-08-23 17:33:54 +0100

    new next commit 12

--- inheritance/town.py
+++ inheritance/town.py
@@ -1,13 +1,15 @@
 # nie zakładamy sprawdzania zgodności typów
 class Town:
-    def __init__(self, name, county):
+    def __init__(self, name, county, firstname):
         self.__name = name  # zmienne prywatne, niedostępne do modyfikacji po zainicjowaniu
         self.__county = county
+        self.firstname = firstname
 
     def location(self):
         print(self.__name + ", powiat " + self.__county)
         print('To jest powiat: ' + self.__county)
         print('To województwo: ' + self.__name)
+        print('Mam na  imie: ' + self.firstname + ', mój powiat to: ' + self.__county + ', województwo: ' + self.__name)
 
     def _county(self):
         '''metoda z założenia chroniona jedynie do wykorzystania w klasach potomnych'''
@@ -19,16 +21,15 @@ class Town:
 
 
 if __name__ == '__main__':
-    town1 = Town("Luboń", "Poznański")
-    town2 = Town("Pomorskie", "Gdansk")
-    town3 = Town("Slaskie", "Wrocław")
+    town1 = Town("Luboń", "Poznański", "Jacek")
+    # town2 = Town("Pomorskie", "Gdansk", "Bartek")
+    # town3 = Town("Slaskie", "Wrocław", "Ala")
     town1.location()
-    town2.location()
-    town3.location()
+    # town2.location()
+    # town3.location()
     print(type(town1))
-    print(type(town2))
-    print(type(town3))
-
+    # print(type(town2))
+    # print(type(town3))
 
     # Luboń, county poznański
     # Pomorskie, county Gdansk
